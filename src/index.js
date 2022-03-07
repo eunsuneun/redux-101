f7f0351a1b2c62a2476c08b5dc47c10171e9f6fb
eunsuneun <eunsunbsn@gmail.com>
2022-03-07 14:51:05 +0900

    count application only vanillaJS

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const plus = document.getElementById('plus');
+const minus = document.querySelector('#minus');
+const number = document.querySelector('span');
+
+let count = 0;
+
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handlePlus = () => {
+  count++;
+  updateText();
+};
+
+const handleMinus = () => {
+  count--;
+  updateText();
+};
+
+plus.addEventListener('click', handlePlus);
+minus.addEventListener('click', handleMinus);
