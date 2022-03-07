6dd570b23a3741ef5654d66ab37004dcef069394
eunsuneun <eunsunbsn@gmail.com>
2022-03-07 20:51:55 +0900

    count application with Redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById('plus');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
 
-let count = 0;
+const PLUS = "PLUS";
+const MINUS = "MINUS";
 
-number.innerText = count;
+const reducer = (count = 0, action) => {
+  switch (action.type) {
+    case PLUS: 
+      return count + 1;
+    case MINUS: 
+      return count - 1;
+    default:
+      return count;
+  }
+};
+const store = createStore(reducer);
 
-const updateText = () => {
-  number.innerText = count;
+const onChange = () => {
+  number.innerText = store.getState();
 };
 
+store.subscribe(onChange);
+
 const handlePlus = () => {
-  count++;
-  updateText();
+  store.dispatch({type: PLUS});
 };
 
 const handleMinus = () => {
-  count--;
-  updateText();
+  store.dispatch({type: MINUS});
 };
 
 plus.addEventListener('click', handlePlus);
